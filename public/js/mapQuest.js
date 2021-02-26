058185a01412b1d9b83e057f433a96e1f3d82645
sophiaboss11 <51395437+sophiaboss11@users.noreply.github.com>
2021-02-25 21:04:13 -0800

    initial

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = '1jkmtxPpewLw5AAQ5tLbF0AJsarXvLjD';
+
+	// 'map' refers to a <div> element with the ID map
+	L.mapquest.map('map', {
+	center: [37.7749, -122.4194],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12
+	});
 }
